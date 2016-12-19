66130eff2b04f5427585f5c1e6ee2996d79972d5
marcandy <marcandy.nj@gmail.com>
2016-12-19 16:12:40 -0700

    review

--- js/app.js
+++ js/app.js
@@ -4,7 +4,29 @@ angular.module('devmtnTravel', ['ui.router'])
             .state('home',{
                 url:'/',
                 templateUrl: "../views/about.html"
-            });
+            })
+            .state('packages',{
+                url:'/packages',
+                templateUrl: "../views/packages.html"
+            })
+            .state('locactions',{
+                url:'/locactions',
+                templateUrl: "../views/locactions.html"
+            })
+            .state('booked',{
+                url:'/booked/:id',
+                templateUrl: "../views/booked.html"
+            })
+            .state('adventurers',{
+                url:'/adventurers/',
+                templateUrl: "../views/adventurers.html"
+            })
+            .state('contact',{
+                url:'/contact',
+                templateUrl: "../views/contact.html"
+            })
+
+
 
         $urlRouterProvider
             .otherwise('/');
